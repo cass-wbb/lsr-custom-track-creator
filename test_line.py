7cb01de0f2ef59609faf4771eaed6ea39a645b5f
Cass-Adams <cadams@iwu.edu>
2025-06-01 09:57:03 -0500

    test_line creates blank hex lines to be used.

---
+++ test_line.py
@@ -0,0 +1,40 @@
+import file_create
+
+def create_blank_lines() -> list[list[str]]:
+    """ Creates blank lines to be used for track pieces
+    This function works only for single player tracks.
+
+    :return: The list of blank lines, represented in hex
+    """
+    lines = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    for element in lines:
+        i = 0
+        while i < 16:
+            element.append("00 00 00 00 00 00 80 BF FF FF FF FF 00 00 00 00")
+            i += 1
+    return lines
+
+def biome_select(biome: str) -> str:
+    """ Selects a biome for the background
+    This function only works for single player tracks.
+    
+    :param biome: The name of the biome to use for this level.
+    :return: the single player biome header
+    """
+    if biome == "city":
+        return "28 00 01 00 01 00 00 00 03 00 00 00 00 00 00 00"
+    elif biome == "desert":
+        return "28 00 01 00 01 00 00 00 02 00 00 00 00 00 00 00"
+    elif biome == "jungle":
+        return "28 00 01 00 01 00 00 00 00 00 00 00 00 00 00 00"
+    elif biome == "winter":
+        return "28 00 01 00 01 00 00 00 01 00 00 00 00 00 00 00"
+    else:
+        raise ValueError("Biome not available. Please type one of the following:\ncity, desert, jungle, winter")
+
+def main():
+    lines = create_blank_lines()
+    biome = biome_select("city")
+
+if __name__ == "__main__":
+    main()
